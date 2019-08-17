58ce9d20a616ae285bd75d4fa844e07613a47549
internetchris <chris@ookla.com>
2019-08-17 07:52:29 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# temp_json_parser
-temporary parser scripts
+# Temporary Parser
+
+## Requirements
+1. compareReports.json
+1. node.js
+
+The SQL query to output the compareReports.json is as follows:
+
+
+    SELECT users.data, users.customer_id, c.customer_name, c.customer_id FROM auth0_users users LEFT JOIN customers c
+           ON users.customer_id=c.customer_id
+
+## Output
+
+This script will output the following files:
+
+- **allReports.json**: This is a compliation of all compare reports in JSON format
+- **maxComparators.json**: This is a compilation of all compare reports that exceed the maxComparator param (currently 15).
+- **maxMultiSelects.json**: This is a compilation of all compare reports that exceed the maxSelects param (currently 2).
+
+- **errors.json**: Any errors that were generated when parsing the compareReports.json file.
---
+++ parse.js
@@ -0,0 +1,175 @@
+var json = require("./compareReports.json");
+var fs = require('fs');
+var _ = require('./lodash');
+
+function analyzeComparators(comparators, multiSelectMax, totalMax) {
+   var compares = comparators.map(c => {
+     c.location = c.location.id;
+     c.provider = c.provider.id || undefined;
+     c.device = c.device ? c.device.id || undefined : undefined;
+     return c;
+   })
+
+   // merge all comparator objects to get possible keys.
+   // [bandwidth_metric_type, mobile_market_type, provider, location]
+   var result =  _.assign.apply(_, compares);
+   var keys = Object.keys(result);
+   var combined = {};
+
+   // combine all possible keys into arrays.
+   keys.map(key => {
+     combined[key] = [...new Set(compares.filter(c => {
+       return c[key] !== undefined;
+     }).map(it => it[key]))];
+   })
+
+   // calculate total comparators and multiselects
+   var multiSelects = 0;
+   var totalComparators = 0;
+
+   _.forEach(combined, function(value, key) {
+     totalComparators = totalComparators + value.length;
+     if (value.length > 1) {
+       multiSelects = multiSelects + 1;
+     }
+   });
+
+   // Breakup information that's returned in useful chunks.
+
+   var maxComparators = {};
+   var maxMultiSelects = {};
+
+   var combinedFilters = {
+     filters: combined,
+   }
+   var comparatorCounts = {
+     totalComparators: totalComparators,
+     totalMultiSelects: multiSelects,
+   }
+   var rawReports = {
+     rawReports: comparators,
+   };
+
+   if (totalComparators > totalMax) {
+     maxComparators = {
+       filters: combined,
+       totalComparators: totalComparators,
+       totalMultiSelects: multiSelects,
+     }
+   }
+
+   if ( multiSelects > multiSelectMax) {
+     maxMultiSelects = {
+       filters: combined,
+       totalComparators: totalComparators,
+       totalMultiSelects: multiSelects,
+     }
+   }
+
+   var compare = {
+     combinedFilters,
+     comparatorCounts,
+     maxComparators,
+     maxMultiSelects,
+     rawReports
+   }
+
+   return compare;
+}
+
+
+
+var count = 0;
+var allInfo = {};
+var maxComparators = {};
+var maxMultiSelects = {};
+var errors = {};
+var urls = [];
+
+// parse json from DB
+for (var i = 0; i < json.length; i++) {
+    var data = JSON.parse(json[i].data)
+    var customerName = json[i].customer_name;
+
+    if (customerName) {
+      var customerName = json[i].customer_name.split(' ').join('_');
+
+      // Saved Reports
+      var saved = data.savedReportsGoogle;
+      if (saved && saved.length) {
+        var nameKey = json[i].customer_id + '_' + customerName;
+
+        // setup initial object for customer information
+        var customer = allInfo[nameKey];
+        if (!customer) {
+          allInfo[nameKey] = {
+            name:json[i].customer_name,
+            compareReportCount: 0,
+            compareReports: []
+          }
+        }
+
+
+        // Iteration through the saved reports of a single customer.
+        for (var s = 0; s < saved.length; s++) {
+          var savedReport = saved[s];
+
+          // Only look at compare reports
+          if (savedReport.type === 'compare') {
+
+            // attempt to parse the url into json.
+            var params = savedReport.url.replace('#compare', '').replace('?', '');
+            if (params !== '') {
+              try {
+                urlParams = JSON.parse('{"' + params.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) });
+                // parse comparators to be more readable.
+                urlParams.comparators = JSON.parse(urlParams.comparators);
+
+                // iteration comparators to determine if we have any metrics that have been exceeded by the limitations of the new UI.
+                // params(comparators, maxMultiSelects, maxComparators);
+                var analyzed = analyzeComparators(urlParams.comparators, 2, 15);
+
+                if (analyzed.maxComparators && analyzed.maxComparators.filters) {
+                  maxComparators[nameKey] = {
+                    name:json[i].customer_name,
+                    ...analyzed.maxComparators
+                  }
+                }
+
+                if (analyzed.maxMultiSelects && analyzed.maxMultiSelects.filters) {
+                  maxMultiSelects[nameKey] = {
+                    name:json[i].customer_name,
+                    ...analyzed.maxMultiSelects
+                  }
+                }
+
+                urlParams.comparators = {
+                  ...analyzed.combinedFilters,
+                  ...analyzed.comparatorCounts,
+                  ...analyzed.rawReports
+                }
+
+
+                allInfo[nameKey].compareReportCount = allInfo[nameKey].compareReportCount + 1;
+                allInfo[nameKey].compareReports.push(urlParams);
+                count = count + 1;
+              } catch (e) {
+                errors[nameKey] = {
+                  reportWithError: savedReport,
+                  errorMessage: e.message
+                }
+              }
+
+            }
+          }
+        }
+      }
+    }
+
+}
+
+fs.writeFileSync('allReports.json', JSON.stringify(allInfo, null, 4));
+fs.writeFileSync('maxComparators.json', JSON.stringify(maxComparators, null, 4));
+fs.writeFileSync('maxMultiSelects.json', JSON.stringify(maxMultiSelects, null, 4));
+
+fs.writeFileSync('errors.json', JSON.stringify(errors, null, 4));
